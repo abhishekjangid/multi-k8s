fbd512b55ea48495dd1729d6c5c9b88e96ba7909
unknown <abhishekp.jangid@gmail.com>
2020-05-27 23:46:56 +0530

    Updated client

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,8 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
+          <header>
+            <h1 className="App-title">Fib Calculator version by Abhishek Jangid</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
